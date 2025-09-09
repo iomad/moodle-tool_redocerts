5d0a192a831eae2c6deca0a68c6408fb32c58186
Derick Turner <derick@e-learndesign.co.uk>
2025-09-09 18:05:40 +0100

    IOMAD: update tool_redocerts to pass local codechecker plugin PHP CS checks #2486

--- version.php
+++ version.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Version information
+ *
  * @package   tool_redocerts
  * @copyright 2021 Derick Turner
  * @author    Derick Turner
@@ -23,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = '4.5.6 (Build: 20250811)'; // Human-friendly version name
+$plugin->release  = '4.5.6 (Build: 20250811)'; // Human-friendly version name.
 $plugin->version  = 2025071645;   // The (date) version of this plugin.
 $plugin->requires = 2024100700;   // Requires this Moodle version.
-$plugin->component = 'tool_redocerts'; // Full name of the plugin (used for diagnostics)
+$plugin->component = 'tool_redocerts'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = ['local_iomad' => 2024090401];
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
